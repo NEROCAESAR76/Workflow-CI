d59ec250dda7b2fefe9fa56804ca299b8987f257
NEROCAESAR76 <nerosuprobo@gmail.com>
2026-06-08 13:02:07 +0700

    Fix MLflow experiment clash and update path

--- modelling.py
+++ modelling.py
@@ -4,11 +4,6 @@ import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import os
-
-# Set tracking ke lokal (wajib buat bikin Docker nanti)
-mlflow.set_tracking_uri("file://" + os.path.abspath("mlruns"))
-mlflow.set_experiment("Telco_Churn_CI")
 
 print("Membaca data...")
 df = pd.read_csv('telco_churn_clean.csv')
@@ -19,6 +14,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
 
+# Langsung start run aja, biarkan robot MLflow yang atur folder mlruns-nya
 with mlflow.start_run():
     print("Melatih model...")
     model.fit(X_train, y_train)
@@ -26,7 +22,6 @@ with mlflow.start_run():
     acc = accuracy_score(y_test, y_pred)
     
     mlflow.log_metric("accuracy", acc)
-    # Nama folder modelnya: random_forest_model
     mlflow.sklearn.log_model(model, "random_forest_model")
     
     print(f"Selesai! Akurasi: {acc:.2f}")
